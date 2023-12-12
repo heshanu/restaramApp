a080dd94522d2a1545b36cff63196581f39ad666
heshanu <heshanu97@gmail.com>
2023-12-12 12:24:31 +0530

    Added frontEnd

---
+++ frontEnd/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ja_JP } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ja from '@angular/common/locales/ja';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(ja);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    provideClientHydration(),
+    { provide: NZ_I18N, useValue: ja_JP }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ restaturant.springboot/src/main/java/com/restaturant/restaturant/springboot/entity/UserEntity.java
@@ -0,0 +1,9 @@
+package com.restaturant.restaturant.springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+}
