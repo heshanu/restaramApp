574ca7bb140ad6c06c1f8294c79947e7d44a6d4d
heshanu <heshanu97@gmail.com>
2023-12-22 20:16:49 +0530

    updated

--- restaturant.springboot/src/main/java/com/restaturant/restaturant/springboot/serviceImpl/UserServiceImpl.java
+++ restaturant.springboot/src/main/java/com/restaturant/restaturant/springboot/serviceImpl/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package com.restaturant.restaturant.springboot.serviceImpl;
 
+import com.restaturant.restaturant.springboot.entity.UserEntity;
+import com.restaturant.restaturant.springboot.entity.test;
 import com.restaturant.restaturant.springboot.repo.UserRepo;
 import com.restaturant.restaturant.springboot.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -25,5 +30,16 @@ public class UserServiceImpl implements UserService {
                         .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
             }
         };
+    }
+
+    @Override
+    public ResponseEntity<List<UserEntity>> findAll() {
+        try{
+            return (ResponseEntity<List<UserEntity>>) userRepository.findAll();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(null);
     }
 }
